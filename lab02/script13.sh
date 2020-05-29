9dd0e42c12e606998bdf4dd83049f71d513dfeee
kowalik-lukasz <48880659+kowalik-lukasz@users.noreply.github.com>
2020-05-29 19:46:40 +0200

    Update script13.sh

--- lab02/script13.sh
+++ lab02/script13.sh
@@ -1,20 +1,21 @@
 # ! /bin/bash
 
-# Sprawdzenie poprawności liczby argumentów.
+# Checking the integrity of script arguments
+# Checking the no. of arguments
 if [ $# -lt 2 ]
 then
    echo "Error! Insufficient no. of arguments."
    exit 1;
 fi
 
-# Sprawdzenie czy podane katalogi istnieją.
+# Checking if given directories exist
 if [ ! -d $1 -o ! -d $2 ]
 then
    echo "Error! At least one of the provided directories does not exist within this scope."
    exit 1;
 fi
 
-# Pętla sprawdzająca czy dany element z $2 jest plikiem oraz czy jego nazwa powiela się w $1. Jeśli tak, zostaje usunięty z $1.
+# A loop iterating over elements from $2 checking if they're files and if its name is duplicated in $1. If so, it gets deleted from $1.
 for i in `ls $2`
 do
    if [ -f $2/$i -a -f $1/$i ]
